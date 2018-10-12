c5db4b6af341459021c2513885e615813dc75bbd
Vincent Foti <Vincent.Foti@wakefern.com>
2018-10-12 10:07:50 -0400

    build: init load of modules

---
+++ tools/release.js
@@ -0,0 +1,6 @@
+require('dotenv').config(); // load .env
+
+require('semantic-release')(); // call semantic-release
+
+// If your using external logging this would be a good place to kick off
+// you sourcemap upload.
